e1aafff9c7d8d6a8ffcac532ad3a85d2487e2e09
Lucas Ambesi <ambesilucas@gmail.com>
2022-06-07 20:21:06 -0300

    refactor logo

--- src/App.js
+++ src/App.js
@@ -3,19 +3,15 @@ import { Provider } from 'react-redux'
 import Players from './components/common/Players'
 import store from './Store/store'
 import { SelectedTeam } from './components/common/SelectedTeam'
-
-import logossc from '../src/assets/logossc.jpg'
+import { Logo } from './components/common/Logo'
 
 import './styles/styles.scss'
-import './styles/ssclogo.scss'
 
 export const App = () => {
   return (
       <Provider store={store}>
-        <main>          
-          <div className='logo-ssc'>
-              <img src={logossc} alt='logossc'/>                 
-          </div>      
+        <main>       
+          <Logo/>        
           <Players/>
           <SelectedTeam/>
         </main>
---
+++ src/components/common/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import logossc from '../../assets/logossc.jpg'
+
+import '../../styles/ssclogo.scss'
+
+export const Logo = () => {
+  return (
+    <div className='logo-ssc'>
+        <img src={logossc} alt='logossc'/>                 
+    </div> 
+  )
+}
