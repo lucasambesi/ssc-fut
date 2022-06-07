03d6435613602f5d40b66afa21fb1740c62d5e08
Lucas Ambesi <ambesilucas@gmail.com>
2022-06-07 18:25:34 -0300

    create Team component

---
+++ src/components/common/Team.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+import { types } from '../../types/types'
+
+const Team = ({team, players, deleteTeamPlayer}) => {
+  return (
+    <>
+        {
+            players[team].map((player) =>{
+                return(
+                    <article className={team} key={player.id}>
+                        <div>
+                            <img src={player.image} alt={player.name}/>
+                            <button onClick={() => deleteTeamPlayer(player, types.DELETE, team)}>X</button>
+                        </div>
+                        <p>{player.name}</p>
+                    </article>
+                )
+            })
+        }
+    </>
+  )
+}
+
+const mapStateToProps = (state) => ({
+    players: state
+})
+
+const mapDispatchToProps = (dispatch) => ({
+    deleteTeamPlayer(player, type, team){
+        dispatch({
+            type: type,
+            payload: {
+                player: player,
+                team: team
+            },
+        })
+    }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Team)
