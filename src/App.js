a132e508f1981629d11cfb48a5c13456e065dff2
Lucas Ambesi <ambesilucas@gmail.com>
2022-06-07 19:43:34 -0300

    add logo to App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import { Provider } from 'react-redux'
 
 import Players from './components/common/Players'
-import { SelectedTeam } from './components/common/SelectedTeam'
 import store from './Store/store'
+import { SelectedTeam } from './components/common/SelectedTeam'
+
+import logossc from '../src/assets/logossc.jpg'
 
 import './styles/styles.scss'
+import './styles/ssclogo.scss'
 
 export const App = () => {
   return (
       <Provider store={store}>
-        <main>
-          <h1>SSC</h1>
+        <main>          
+          <div className='logo-ssc'>
+              <img src={logossc} alt='logossc'/>                 
+          </div>      
           <Players/>
           <SelectedTeam/>
         </main>
