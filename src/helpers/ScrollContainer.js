49761d96d90b3603486bbc10967a6ef0aac19eec
Lucas Ambesi <ambesilucas@gmail.com>
2022-06-07 18:26:40 -0300

    create ScrollContainer helper

---
+++ src/helpers/ScrollContainer.js
@@ -0,0 +1,27 @@
+import { createRef } from 'react'
+
+export const gridJugadores = createRef()
+
+export const setScrollContainer = (desktop = true) => {
+    let container = gridJugadores.current
+    if (container) {
+      const generatedGrid = () => {
+        let items = container.getElementsByClassName('jugador')
+        let itemsLength = items.length
+        let bp = window.matchMedia('(min-width: 640px)').matches ? window.matchMedia('(min-width: 1024px)').matches ? 4 : 2 : 1
+
+        const getWidth = () => {
+          let n = bp + (itemsLength > bp ? 0.3 : 0)
+          return (itemsLength / n) * 10
+        }
+        return `
+                display: grid;
+                grid-template-columns: repeat(${itemsLength}, 225px);
+                grid-gap: 1rem;
+                width : ${getWidth()}%;
+              `
+      }
+      let styles = !desktop && window.matchMedia('(min-width: 1024px)').matches ? `display: grid; grid-row-gap: 1rem;` : generatedGrid()
+      container.setAttribute('style', styles)
+    }
+  }
