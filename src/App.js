6760248fe6b3af9b43fb8a25c2735ed0baeff2c0
Lucas Ambesi <ambesilucas@gmail.com>
2022-06-09 23:48:13 -0300

    include SelectionPlayers external in App

--- src/App.js
+++ src/App.js
@@ -6,15 +6,17 @@ import { SelectedTeam } from './components/common/SelectedTeam'
 import { Logo } from './components/common/Logo'
 
 import './styles/styles.scss'
+import { typesPlayer } from './types/types'
 
 export const App = () => {
   return (
-      <Provider store={store}>
-        <main>       
-          <Logo/>        
-          <SelectionPlayers/>
-          <SelectedTeam/>
-        </main>
+      <Provider store={store} >    
+          <main>
+            <Logo/>        
+            <SelectionPlayers/>
+            <SelectedTeam/>
+            <SelectionPlayers type={typesPlayer.EXTERNAL}/>
+          </main>
       </Provider>
   )
 }
